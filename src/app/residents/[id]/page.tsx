cdb20a725e3d87c49473e76a2ea5ad3e63825308
fuchsgerson <fuchsgerson@gmail.com>
2025-01-10 08:12:18 -0300

    style(create/edit residents) cleanup

--- src/app/residents/[id]/page.tsx
+++ src/app/residents/[id]/page.tsx
@@ -154,29 +154,6 @@ const ResidentFormPage: React.FC = () => {
                   value={formData.phone}
                   onChange={handleChange}
                 />
-                {/* <div className="sm:col-span-1">
-                  <label
-                    htmlFor="role"
-                    className="block text-sm/6 font-medium text-gray-900"
-                  >
-                    Função
-                  </label>
-                  <div className="mt-2 grid grid-cols-1">
-                    <select
-                      id="role"
-                      name="role"
-                      className="col-start-1 row-start-1 w-full appearance-none rounded-md bg-white py-1.5 pl-3 pr-8 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                    >
-                      <option>Admin</option>
-                      <option>Recepção</option>
-                      <option>Residente</option>
-                    </select>
-                    <ChevronDownIcon
-                      aria-hidden="true"
-                      className="pointer-events-none col-start-1 row-start-1 mr-2 size-5 self-center justify-self-end text-gray-500 sm:size-4"
-                    />
-                  </div>
-                </div> */}
                 <SelectField
                   id="role"
                   name="role"
