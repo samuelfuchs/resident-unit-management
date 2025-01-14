35dbaf8c0c80a91f1b273eed35e520f83b02db9f
fuchsgerson <fuchsgerson@gmail.com>
2025-01-14 20:35:30 -0300

    deploy commit

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080/api",
+  baseURL: "https://resident-unit-management-backend.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
