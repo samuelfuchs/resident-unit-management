759e0be4dd81a1d12d0491473ac2f041fa838fc7
fuchsgerson <fuchsgerson@gmail.com>
2025-01-15 21:17:51 -0300

    style(Login) cleanup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,6 @@ const LoginPage: React.FC = () => {
   const [error, setError] = useState<string>("");
   const [showPassword, setShowPassword] = useState<boolean>(false);
   const [loading, setLoading] = useState<boolean>(false);
-  console.log("loading", loading);
 
   useEffect(() => {
     if (user) {
@@ -89,38 +88,6 @@ const LoginPage: React.FC = () => {
               </button>
             </div>
           </div>
-          {/* <button
-            type="submit"
-            disabled={loading}
-            className={`w-full flex items-center justify-center bg-blue-500 text-white py-2 rounded hover:bg-blue-600 ${
-              loading ? "opacity-75 cursor-not-allowed" : ""
-            }`}
-          >
-            {loading ? (
-              <svg
-                className="animate-spin h-5 w-5 text-white"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-              >
-                <circle
-                  className="opacity-25"
-                  cx="12"
-                  cy="12"
-                  r="10"
-                  stroke="currentColor"
-                  strokeWidth="4"
-                ></circle>
-                <path
-                  className="opacity-75"
-                  fill="currentColor"
-                  d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"
-                ></path>
-              </svg>
-            ) : (
-              "Acessar"
-            )}
-          </button> */}
           <Button type="submit" loading={loading} className="w-full">
             Acessar
           </Button>
