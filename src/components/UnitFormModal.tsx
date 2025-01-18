31d407b6dcf8e3d465643a7f7cd5d5165afe5632
fuchsgerson <fuchsgerson@gmail.com>
2025-01-18 10:32:38 -0300

    style(UnitFormModal) improved styling

--- src/components/UnitFormModal.tsx
+++ src/components/UnitFormModal.tsx
@@ -47,12 +47,9 @@ const UnitFormModal: React.FC<UnitFormModalProps> = ({
   const [selectedOwners, setSelectedOwners] = useState<
     { value: string; label: string }[]
   >([]);
-  console.log("unit", unit);
-
   const [selectedTenants, setSelectedTenants] = useState<
     { value: string; label: string }[]
   >([]);
-
   const [formData, setFormData] = useState<Partial<Unit>>({
     number: "",
     floor: 0,
@@ -62,24 +59,55 @@ const UnitFormModal: React.FC<UnitFormModalProps> = ({
     parkingSpots: [],
     tenant: [],
   });
-
-  const [hasUnsavedChanges, setHasUnsavedChanges] = useState(false);
   const modalRef = useRef<HTMLDivElement>(null);
-
   const isViewMode = mode === "view";
-  const isEditMode = mode === "edit";
-  const isCreateMode = mode === "create";
+  const isCreateOrEditMode = mode === "create" || mode === "edit";
 
-  const handleClickOutside = (event: MouseEvent) => {
-    if (
-      modalRef.current &&
-      !modalRef.current.contains(event.target as Node) &&
-      !hasUnsavedChanges
-    ) {
-      onClose();
+  useEffect(() => {
+    if (unit) {
+      setFormData(unit);
+      setSelectedOwners(
+        unit.owner?.map((user) => ({
+          value: user._id,
+          label: `${user.name} ${user.lastName}`,
+        })) || []
+      );
+      setSelectedTenants(
+        unit.tenant?.map((user) => ({
+          value: user._id,
+          label: `${user.name} ${user.lastName}`,
+        })) || []
+      );
+    } else {
+      resetForm();
     }
+  }, [unit]);
+
+  const resetForm = () => {
+    setFormData({
+      number: "",
+      floor: 0,
+      squareFootage: 0,
+      type: UnitType.Residential,
+      owner: [],
+      parkingSpots: [],
+      tenant: [],
+    });
+    setSelectedOwners([]);
+    setSelectedTenants([]);
   };
-  const fetchTenantOptions = async (inputValue: string) => {
+
+  const isFormValid =
+    formData.number &&
+    formData.floor !== undefined &&
+    formData.squareFootage !== undefined &&
+    formData.type &&
+    selectedOwners.length > 0;
+
+  const fetchOptions = async (
+    inputValue: string,
+    roleFilter?: string
+  ): Promise<{ value: string; label: string }[]> => {
     try {
       const { users } = await fetchUsers({
         search: inputValue,
@@ -87,130 +115,43 @@ const UnitFormModal: React.FC<UnitFormModalProps> = ({
         limit: 10,
         sortField: "name",
         sortOrder: "asc",
+        role: roleFilter,
       });
-      return users.map((user: any) => ({
+      return users.map((user: User) => ({
         value: user._id,
         label: `${user.name} ${user.lastName}`,
       }));
     } catch (error) {
-      console.error("Failed to fetch tenant options:", error);
+      console.error("Failed to fetch options:", error);
       return [];
     }
   };
-
-  const fetchOwnerOptions = async (inputValue: string) => {
-    try {
-      const response = await fetchUsers({
-        search: inputValue,
-        page: 1,
-        limit: 10,
-        sortField: "name",
-        sortOrder: "asc",
-      });
-      return response.users.map((user: any) => ({
-        value: user._id,
-        label: `${user.name} ${user.lastName}`,
-      }));
-    } catch (error) {
-      console.error("Failed to fetch owner options:", error);
-      return [];
-    }
-  };
-
-  useEffect(() => {
-    if (unit) {
-      setFormData(unit);
-
-      // Populate selectedOwners state
-      if (unit.owner) {
-        setSelectedOwners(
-          unit.owner.map((user) => ({
-            value: user._id,
-            label: `${user.name} ${user.lastName}`,
-          }))
-        );
-      }
-
-      // Populate selectedTenants state
-      if (unit.tenant) {
-        setSelectedTenants(
-          unit.tenant.map((user) => ({
-            value: user._id,
-            label: `${user.name} ${user.lastName}`,
-          }))
-        );
-      }
-    } else {
-      setFormData({
-        number: "",
-        floor: 0,
-        squareFootage: 0,
-        type: UnitType.Residential,
-        owner: [],
-        parkingSpots: [],
-        tenant: [],
-      });
-      setSelectedOwners([]);
-      setSelectedTenants([]);
-    }
-  }, [unit]);
-
-  useEffect(() => {
-    if (!isOpen) return;
-
-    if ((isViewMode || isCreateMode) && !hasUnsavedChanges) {
-      document.addEventListener("mousedown", handleClickOutside);
-    } else {
-      document.removeEventListener("mousedown", handleClickOutside);
-    }
-
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, [isOpen, isViewMode, isCreateMode, hasUnsavedChanges]);
-
-  useEffect(() => {
-    if (unit) {
-      setFormData(unit);
-    } else {
-      setFormData({
-        number: "",
-        floor: 0,
-        squareFootage: 0,
-        type: UnitType.Residential,
-        owner: [],
-        parkingSpots: [],
-        tenant: [],
-      });
-    }
-  }, [unit]);
 
   const handleChange = (
     e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
   ) => {
     const { name, value } = e.target;
     setFormData((prev) => ({ ...prev, [name]: value }));
-    setHasUnsavedChanges(true);
   };
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     const payload = {
       number: formData.number || "",
-      floor: formData.floor || 0,
-      squareFootage: formData.squareFootage || 0,
+      floor: formData.floor ?? 0,
+      squareFootage: formData.squareFootage ?? 0,
       type: formData.type || UnitType.Residential,
       owner: selectedOwners.map((owner) => owner.value),
       parkingSpots: formData.parkingSpots || [],
       tenant: selectedTenants.map((tenant) => tenant.value),
     };
+
     try {
       if (unit?._id) {
         await updateUnit(unit._id, payload);
       } else {
         await createUnit(payload);
       }
-      setHasUnsavedChanges(false);
       onSubmitSuccess();
       onClose();
     } catch (error) {
@@ -227,87 +168,89 @@ const UnitFormModal: React.FC<UnitFormModalProps> = ({
         className="bg-white rounded-lg shadow-lg w-full max-w-lg p-6"
       >
         <h2 className="text-lg font-bold mb-4">
-          {isCreateMode && "Criar Nova Unidade"}
-          {isEditMode && "Editar Unidade"}
-          {isViewMode && "Visualizar Unidade"}
+          {isCreateOrEditMode
+            ? mode === "create"
+              ? "Criar Nova Unidade"
+              : "Editar Unidade"
+            : "Visualizar Unidade"}
         </h2>
-        <form
-          className="space-y-4"
-          onSubmit={(e) => {
-            e.preventDefault();
-            if (!isViewMode) {
-              handleSubmit(e);
-            }
-          }}
-        >
-          <InputField
-            id="number"
-            name="number"
-            label="Número"
-            value={formData.number || ""}
-            onChange={handleChange}
-            disabled={isViewMode}
-            required={!isViewMode}
-          />
-          <InputField
-            id="floor"
-            name="floor"
-            label="Andar"
-            type="number"
-            value={formData.floor?.toString() || ""}
-            onChange={handleChange}
-            disabled={isViewMode}
-          />
-          <InputField
-            id="squareFootage"
-            name="squareFootage"
-            label="Tamanho (m²)"
-            type="number"
-            value={formData.squareFootage?.toString() || ""}
-            onChange={(e) =>
-              setFormData((prev) => ({
-                ...prev,
-                squareFootage: parseInt(e.target.value, 10) || 0,
-              }))
-            }
-            disabled={isViewMode}
-            required={!isViewMode}
-          />
-          <SelectField
-            id="type"
-            name="type"
-            label="Tipo"
-            value={formData.type || UnitType.Residential}
-            onChange={handleChange}
-            options={Object.values(UnitType).map((type) => ({
-              value: type,
-              label: type,
-            }))}
-            disabled={isViewMode}
-          />
+        <form className="space-y-4" onSubmit={handleSubmit}>
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+            <InputField
+              id="number"
+              name="number"
+              label="Número"
+              value={formData.number || ""}
+              onChange={handleChange}
+              disabled={isViewMode}
+              required={!isViewMode}
+            />
+            <InputField
+              id="floor"
+              name="floor"
+              label="Andar"
+              type="number"
+              value={formData.floor?.toString() || ""}
+              onChange={handleChange}
+              disabled={isViewMode}
+              required={!isViewMode}
+            />
+          </div>
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+            <InputField
+              id="squareFootage"
+              name="squareFootage"
+              label="Tamanho (m²)"
+              type="number"
+              value={formData.squareFootage?.toString() || ""}
+              onChange={(e) =>
+                setFormData((prev) => ({
+                  ...prev,
+                  squareFootage: parseInt(e.target.value, 10) || 0,
+                }))
+              }
+              disabled={isViewMode}
+              required={!isViewMode}
+            />
+
+            <SelectField
+              id="type"
+              name="type"
+              label="Tipo"
+              value={formData.type || UnitType.Residential}
+              onChange={handleChange}
+              options={Object.values(UnitType).map((type) => ({
+                value: type,
+                label: type,
+              }))}
+              disabled={isViewMode}
+              required
+            />
+          </div>
 
           <div>
             <label
               htmlFor="owner-select"
-              className="block text-sm font-medium text-gray-700"
+              className="block text-sm font-medium mb-2"
             >
-              Proprietário
+              Proprietário *
             </label>
             <AsyncSelect
               isMulti
               cacheOptions
               defaultOptions
-              loadOptions={fetchOwnerOptions}
+              loadOptions={(input) => fetchOptions(input)}
               value={selectedOwners}
-              onChange={(newValue) => setSelectedOwners([...newValue])}
+              onChange={(value) => setSelectedOwners([...value])}
               isDisabled={isViewMode}
-              placeholder="Search and select owners..."
+              placeholder="Selecione os proprietários"
             />
           </div>
+
           <div>
             <label
               htmlFor="tenant-select"
-              className="block text-sm font-medium text-gray-700"
+              className="block text-sm font-medium mb-2"
             >
               Inquilinos
             </label>
@@ -315,11 +258,11 @@ const UnitFormModal: React.FC<UnitFormModalProps> = ({
               isMulti
               cacheOptions
               defaultOptions
-              loadOptions={fetchTenantOptions}
+              loadOptions={(input) => fetchOptions(input)}
               value={selectedTenants}
-              onChange={(newValue) => setSelectedTenants([...newValue])}
+              onChange={(value) => setSelectedTenants([...value])}
               isDisabled={isViewMode}
-              placeholder="Search and select tenants..."
+              placeholder="Selecione os inquilinos"
             />
           </div>
           <InputField
@@ -341,8 +284,10 @@ const UnitFormModal: React.FC<UnitFormModalProps> = ({
             <Button variant="secondary" onClick={onClose}>
               {isViewMode ? "Fechar" : "Cancelar"}
             </Button>
-            {!isViewMode && (
-              <Button type="submit">{isCreateMode ? "Criar" : "Salvar"}</Button>
+            {isCreateOrEditMode && (
+              <Button type="submit" disabled={!isFormValid}>
+                {mode === "create" ? "Criar" : "Salvar"}
+              </Button>
             )}
           </div>
         </form>
